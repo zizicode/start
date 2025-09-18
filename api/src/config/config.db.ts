75778755fd8347ed6a8c05f60140afc903087f8c
zizicode <victordoblezz@gmail.com>
2025-09-18 03:32:27 -0400

    updated config

--- api/src/config/config.db.ts
+++ api/src/config/config.db.ts
@@ -8,7 +8,7 @@ export const pool = new Pool({
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
 });
 
 // Prueba de conexión opcional
